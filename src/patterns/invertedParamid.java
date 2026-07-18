9a231f53ae8abfdb3d3e753b4af075fc3acd10c8
Soban Abbas <sobanabbass@gmail.com>
2026-07-18 13:56:14 +0500

    inverted paramid

--- src/patterns/invertedParamid.java
+++ src/patterns/invertedParamid.java
@@ -2,15 +2,15 @@ package patterns;
 
 import java.util.Scanner;
 
-public abstract class paramid {
-     public static void main(String[] args) {
+public class invertedParamid {
+         public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter rows : ");
         int totalNumberOfRows = sc.nextInt();
 
-        for(int rows=1;rows<=totalNumberOfRows;rows++){
+        for(int rows=totalNumberOfRows;rows>=1;rows--){
             for(int spaces=1;spaces<=totalNumberOfRows-rows;spaces++){
-System.out.print(" ");
+                System.out.print(" ");
             }
             for(int stars=1;stars<=rows+(rows-1);stars++){
                 System.out.print("*");
@@ -18,8 +18,6 @@ System.out.print(" ");
             System.out.println("");
         }
 
-
-        
         sc.close();
-}
+         }
 }
